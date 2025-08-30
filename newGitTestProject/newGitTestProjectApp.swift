12bbc16e397a4de6c2b852561b0fb5c5dbfa0111
Nikita Tsomuk <123371636+tsomuk@users.noreply.github.com>
2025-08-30 19:14:06 +0300

    Initial Commit

---
+++ newGitTestProject/newGitTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newGitTestProjectApp.swift
+//  newGitTestProject
+//
+//  Created by Nikita Tsomuk on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct newGitTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
